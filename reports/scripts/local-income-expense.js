fff079a46381af667118d8f50d3090b5605a7ad9
wongpratan <wongpratan@hotmail.com>
2024-04-25 16:30:40 +0700

    *Fix RC options to remove 1 : 100 - https://github.com/digi-serve/ns_app/issues/431\#issuecomment-2076503437

--- reports/scripts/local-income-expense.js
+++ reports/scripts/local-income-expense.js
@@ -2,7 +2,15 @@ const title = "<%= title[languageCode] %>",
    domId = "local-income-expense-report",
    frameId = "local-income-expense-report-frame";
 
-const optInstance = new TeamRcFyOptions(title, domId, frameId, { includeEnd: true, allRC: true });
+const optInstance = new TeamRcFyOptions(title, domId, frameId, {
+   includeEnd: true,
+   allRC: true,
+   filterRC: {
+      key: "RC Name",
+      rule: "not_contains",
+      value: "01 : 100",
+   },
+});
 optInstance.generateUI();
 optInstance.getURL = ({ team, rc, start, end }) =>
    `/report/local-income-expense?Teams=${team}&RCs=${rc}&start=${start}&end=${end}`;
